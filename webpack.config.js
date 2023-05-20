30914f077b0e8413ee6a4a68985fd60cc0205161
coolkechang <coolke007@gmail.com>
2023-05-20 21:13:34 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -7,5 +8,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     //这里用 hash 的用途是便于添加缓存。缓存是HTTP协议里规定的。
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: '学习webpack',
+    template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        //如果发现.css结尾的文件名。"css-loader"会把文件内容读到JS里面，"style-loader"将其变成style标签放到head里面
+      },
+    ],
   },
 };
